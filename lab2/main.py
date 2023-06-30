128d7b45e775e0a2f9a59bb9fe8928b9c1a2c215
Alex100 <127301003+hacker1384y@users.noreply.github.com>
2023-06-30 15:52:31 +0400

    Update main.py

--- lab2/main.py
+++ lab2/main.py
@@ -1,1 +1,10 @@
-# код лабораторной работы
+global_variable = 0 # объявляем глобальную переменную
+
+def increase_global_variable():
+    global global_variable # используем ключевое слово global, чтобы указать, что хотим использовать глобальную переменную
+    global_variable += 1 # увеличиваем значение глобальной переменной на 1
+    return global_variable # возврат значения
+
+print(increase_global_variable()) # 1
+print(increase_global_variable()) # 2
+print(increase_global_variable()) # 3
