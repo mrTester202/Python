e95aa5bf9e02a161143d065c8ff23a9a7cf6c405
Alex100 <127301003+hacker1384y@users.noreply.github.com>
2023-09-13 20:48:38 +0400

    Update main.py

--- lab3/main.py
+++ lab3/main.py
@@ -1,1 +1,40 @@
-# код лабораторной работы
+class Employee:
+    def __init__(self, name, surname, age):
+        self.name = name
+        self.surname = surname
+        self.age = age
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.__name = name
+
+    def get_surname(self):
+        return self.surname
+
+    def set_surname(self, surname):
+        self.surname = surname
+
+    def get_age(self):
+        return self.age
+
+    def set_age(self, age):
+        self.age = age
+
+
+
+class Manager(Employee):
+    def calculate_bonus(self):
+        return 2000
+
+
+
+class Director(Employee):
+    def calculate_bonus(self):
+        return 6000
+
+
+class DeputyDirector(Employee):
+    def calculate_bonus(self):
+        return 4000
